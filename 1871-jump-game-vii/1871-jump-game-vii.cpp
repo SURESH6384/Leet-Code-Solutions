dbf43a703d1c23eeaf39aa69d967aa10302b5247
SURESH6384 <sureshv14112005@gmail.com>
2026-05-25 18:31:13 +0530

    Time: 8 ms (71.26%), Space: 19.2 MB (97.07%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -3,13 +3,15 @@ public:
     bool canReach(string s, int minJ, int maxJ) {
         int n = s.length();
 
-        if (s.back() & 1) return false;
+        if (s.back() & 1)
+            return false;
 
         s[0] = 'v';
         int reach = 0, maxR = maxJ;
 
         for (int i = minJ; i < n; i++) {
-            if (i > maxR) return false;
+            if (i > maxR)
+                return false;
 
             reach += s[i - minJ] == 'v';
             reach -= (i > maxJ) && s[i - maxJ - 1] == 'v';
